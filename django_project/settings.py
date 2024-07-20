1e5bb987a9e57e520eff47ddcc37147c9837b2e5
CodeYisus12 <166234603+CodeYisus12@users.noreply.github.com>
2024-07-20 16:48:45 -0400

    crispy added

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
